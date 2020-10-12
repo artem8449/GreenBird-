fed85f3903315df0269828c02fdb0f82578f92d5
ArtemVozzgaev <37670156+artem8449@users.noreply.github.com>
2020-10-12 13:39:59 +0300

    Добавил новый функционал
    Добавил функцию для force обьекта

---
+++ Bird/Assets/GreenBird.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class GreenBird : MonoBehaviour
+{
+	Vector3 _initialPosition; 
+	private void Awake()
+	{
+		_initialPosition = transform.position;
+	}
+
+	private void Update()
+	{
+		мтм
+	}
+
+	private void OnMouseDown()
+	{
+		GetComponent<SpriteRenderer>().color = Color.red;
+	}
+
+	private void OnMouseUp()
+	{
+		GetComponent<SpriteRenderer>().color = Color.white;
+		Vector2 directionToInitialPosition = _initialPosition - transform.position;
+		GetComponent<Rigidbody2D>().AddForce(directionToInitialPosition * 100);
+		GetComponent<Rigidbody2D>().gravityScale = 1;
+	}
+
+	private void OnMouseDrag()
+	{
+		Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		transform.position = new Vector3(newPosition.x, newPosition.y);
+	}
+}
